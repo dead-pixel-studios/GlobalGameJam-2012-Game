6d10495a702b98b980428b6485686fb301a33915
Stuart <stuartbrockman@gmail.com>
2012-01-28 16:12:56 +0000

    Improved and cleaned up player code.

--- src/Revival/Player.cpp
+++ src/Revival/Player.cpp
@@ -15,22 +15,18 @@ Player::Player(){
 	_point1=CorePosition(50, 250);
 	_point2=CorePosition(100,250);
 	_origin=_point1;
-
-	_hitFloor=false;
-	_visible=false;
+	_visible=true;
 }
 
 void Player::Update(float){
 	if(IsKeyDown(SDLK_RIGHT)){
-		_visible=false;
-		_hitFloor=false;
 		_pos.SetX(_pos.GetX()+1);
+		if(_pos.GetX()<=0) _pos.SetX(0);
 		_pos.SetY(0);
 	}
 	if(IsKeyDown(SDLK_LEFT)){
-		_visible=false;
-		_hitFloor=false;
 		_pos.SetX(_pos.GetX()-1);
+		if(_pos.GetX()<=0) _pos.SetX(0);
 		_pos.SetY(0);
 	}
 	CorePosition lpoint1=LandPoint(_point1);
@@ -39,9 +35,11 @@ void Player::Update(float){
 	_lpoint2=lpoint2;
 
 	_pos=CorePosition(lpoint1.GetX()-50, lpoint1.GetY()-_size.GetHeight());
-	_visible=true;
 
 	double angle=atan2((double)lpoint2.GetY() - lpoint1.GetY(), (double)lpoint2.GetX() - lpoint1.GetX()) * 180 / 3.14159;
+	std::cout << angle << std::endl;
+	if(angle>60.0) angle=60.0;
+	if(angle<-60.0) angle=-60.0;
 	if(angle<=0)angle+=360.0;
 	_angle=angle;
 }
@@ -49,23 +47,9 @@ void Player::Update(float){
 void Player::Draw(){
 	CorePosition cpoint1=_lpoint1-Universe::Instance()->_worldOffset;
 	CorePosition cpoint2=_lpoint1-Universe::Instance()->_worldOffset;
-	std::cout << cpoint1.GetX() << ':' << cpoint1.GetY() << std::endl;
 	gEngine->DrawRectangle(&cpoint1, &sz33 ,0xff,0,0,0xff);
 	gEngine->DrawRectangle(&cpoint2, &sz33 ,0xff,0,0,0xff);
 	DefaultDraw();
-}
-
-bool Player::WorldCollisionCheck(){
-	static Uint32 lastCol=0;
-
-	CorePosition posToCheck=_point1;
-	posToCheck.SetX(posToCheck.GetX()+_pos.GetX());
-	posToCheck.SetY(posToCheck.GetY()+_pos.GetY());
-
-	CoreColor pxl=Universe::Instance()->_currentMap->GetPixel(posToCheck);
-	Uint32 col=pxl.rgba();
-
-	return (col==0x000000ff);
 }
 
 CorePosition Player::LandPoint(CorePosition point){
--- src/Revival/Player.h
+++ src/Revival/Player.h
@@ -4,12 +4,9 @@
 
 class Player : public SpriteBase{
 private:
-	bool _hitFloor;
-
 	CorePosition _point1, _point2;
 	CorePosition _lpoint1, _lpoint2;
 
-	bool WorldCollisionCheck();
 	CorePosition LandPoint(CorePosition point);
 public:
 	Player();
